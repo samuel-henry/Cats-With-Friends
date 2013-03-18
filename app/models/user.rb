8a7e2942d5aa85d87212b848c808aaf378a4f1f0
Samuel Henry <samuelhenry@gmail.com>
2013-03-18 14:35:10 -0500

    adding fridge

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,11 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password
 
   has_many :phrases, dependent: :destroy
-  has_many :followed_users, through: :relationships, source: :followed
-  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
   
-  has_many :followers, through: :reverse_relationships, source: :follower
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
   has_many :reverse_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
 
 
   has_secure_password
