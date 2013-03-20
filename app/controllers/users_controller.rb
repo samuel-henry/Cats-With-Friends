6121cb97bf5c1b66d83bcaf808b43aca19129e3d
Samuel Henry <samuelhenry@gmail.com>
2013-03-20 13:55:14 -0500

    allow recovery from error when sending welcome email to new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,13 +70,17 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        # Tell the UserMailer to send a welcome Email after save
-        UserMailer.welcome_email(@user).deliver
-        
-        #sign the user in after they create an account
-        session[:user_id] = @user.id
-        format.html { redirect_to @user, notice: 'Your account was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
+        begin
+          # Tell the UserMailer to send a welcome Email after save
+          UserMailer.welcome_email(@user).deliver
+        rescue
+          puts "problem with mailer"
+        ensure
+          #sign the user in after they create an account
+          session[:user_id] = @user.id
+          format.html { redirect_to @user, notice: 'Your account was successfully created.' }
+          format.json { render json: @user, status: :created, location: @user }
+        end
       else
         format.html { render action: "new" }
         format.json { render json: @user.errors, status: :unprocessable_entity }
